733ff184e039669938d16d017d699bac086e7d80
thuynh26 <thuynh26@ucsc.edu>
2025-10-10 13:38:08 +0000

    Added Upgrade Feature + Button (Step 5 Completed)

--- src/main.ts
+++ src/main.ts
@@ -6,45 +6,61 @@ import "./style.css";
 */
 
 let counter: number = 0;
-const ratePerSec: number = 1; // 60fps
+
+let growthRate: number = 0;
+// Initial growth rate -> +1 a purchase is made
+const upgradeCost: number = 10;
 
 document.body.innerHTML = `
   <h1>💞 Total Love Sent: <span id="counter">0</span></h1>
   <button id="button"></button>
   <p>^ Click the button to send love arrows!</p>
-  <button id="autoClickBtn">💓 Rapid Fire</button>
-  <p>(or let it autoclick for you)</p>
+  <button id="upgradeBtn">💓 Rapid Fire</button>
+  <p>Purchase +1 unit upgrade! (Costs 10 Love)</p>
 `;
 
 const cupidBtn = document.getElementById("button")!;
 cupidBtn.textContent = `🏹 Shoot Arrow`;
 
-// Click Handler
-const button = document.getElementById("button")!;
 const counterElement = document.getElementById("counter")!;
+const upgradeBtn = document.getElementById("upgradeBtn")!;
 
-button.addEventListener("click", () => {
+// Manual Button Click Handler
+cupidBtn.addEventListener("click", () => {
   counter++;
   counterElement.textContent = counter.toString();
 });
 
-// Auto Click Handler
-const autoClickBtn = document.getElementById("autoClickBtn")!;
-
-autoClickBtn.addEventListener("click", () => {
-  requestAnimationFrame(autoClick);
-});
-
-// request animationFrame
+// Auto clicker using reqAnimationFrame
 let timeOfLastFrame = performance.now();
+let loopStarted = false;
 
 function autoClick(currentTime: number) {
-  const deltaTime = (currentTime - timeOfLastFrame) / 1000; // Convert to seconds
+  const deltaTime = (currentTime - timeOfLastFrame) / 1000;
   timeOfLastFrame = currentTime;
 
-  const clicksThisFrame = ratePerSec * deltaTime;
+  const clicksThisFrame = growthRate * deltaTime;
   counter += clicksThisFrame;
   counterElement.textContent = Math.floor(counter).toString();
 
   requestAnimationFrame(autoClick);
 }
+
+// Upgrade Handler
+upgradeBtn.addEventListener("click", () => {
+  if (counter >= upgradeCost) {
+    counter -= upgradeCost;
+    growthRate++;
+
+    if (!loopStarted) {
+      loopStarted = true;
+      requestAnimationFrame((t) => {
+        timeOfLastFrame = t;
+        requestAnimationFrame(autoClick);
+      });
+    }
+
+    counterElement.textContent = Math.floor(counter).toString();
+    upgradeBtn.textContent = `💓 Rapid Fire (Currently +${growthRate}/sec)`;
+  }
+});
