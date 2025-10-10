d383e4190c9b3b76525fe2655bebf43867c877be
thuynh26 <thuynh26@ucsc.edu>
2025-10-10 12:16:40 +0000

    Added Button (Step 1 Complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
+/*
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+<p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+*/
+
+const _counter: number = 0;
+
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <button id="button"></button>
 `;
+
+const cupidBtn = document.getElementById("button")!;
+cupidBtn.textContent = `Shoot Arrow 🏹`;
