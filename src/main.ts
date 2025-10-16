94102eb65c67aef77d461082fcfe5b1113aa0b53
thuynh26 <thuynh26@ucsc.edu>
2025-10-16 02:33:45 +0000

    Refactored button functionality logic

--- src/main.ts
+++ src/main.ts
@@ -5,30 +5,47 @@ import "./style.css";
 <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 */
 
+// =============== Game State =============== //
 let counter: number = 0;
 
 let growthRate: number = 0;
 // Initial growth rate -> +1 a purchase is made
+
 const upgradeCost: number = 10;
 
+// =============== DOM =============== //
 document.body.innerHTML = `
   <h1>💞 Total Love Sent: <span id="counter">0</span></h1>
-  <button id="button"></button>
+  <button id="clickBtn">🏹 Shoot Arrow</button>
+
   <p>^ Click the button to send love arrows!</p>
-  <button id="upgradeBtn">💓 Rapid Fire</button>
-  <p>Purchase +1 unit upgrade! (Costs 10 Love)</p>
+
+  <div id="upgradeShop">
+    <button id="upgradeBtnA">💓 Rapid Fire</button>
+    <button id="upgradeBtnB">💓 Upgrade B</button>
+    <button id="upgradeBtnC">💓 Upgrade C</button>
+  </div>
+
+  <p>Purchase unit upgrades!</p>
+
 `;
 
-const cupidBtn = document.getElementById("button")!;
-cupidBtn.textContent = `🏹 Shoot Arrow`;
-
+// =============== DOM ref =============== //
 const counterElement = document.getElementById("counter")!;
-const upgradeBtn = document.getElementById("upgradeBtn")!;
+const clickBtn = document.getElementById("clickBtn")!;
+const upgradeBtnA = document.getElementById("upgradeBtnA")!;
+const upgradeBtnB = document.getElementById("upgradeBtnB")!;
+const upgradeBtnC = document.getElementById("upgradeBtnC")!;
 
-// Manual Button Click Handler
-cupidBtn.addEventListener("click", () => {
-  counter++;
+// =============== Helper Functions =============== //
+function updateCounter() {
   counterElement.textContent = counter.toString();
+}
+
+// =============== Button Handlers =============== //
+clickBtn.addEventListener("click", () => {
+  counter++;
+  updateCounter();
 });
 
 // Auto clicker using reqAnimationFrame
@@ -47,7 +64,7 @@ function autoClick(currentTime: number) {
 }
 
 // Upgrade Handler
-upgradeBtn.addEventListener("click", () => {
+upgradeBtnA.addEventListener("click", () => {
   if (counter >= upgradeCost) {
     counter -= upgradeCost;
     growthRate++;
@@ -60,7 +77,43 @@ upgradeBtn.addEventListener("click", () => {
       });
     }
 
+    updateCounter();
+    upgradeBtnA.textContent = `💓 Rapid Fire (Currently +${growthRate}/sec)`;
+  }
+});
+
+upgradeBtnB.addEventListener("click", () => {
+  if (counter >= 100) {
+    counter -= 100;
+    growthRate++;
+
+    if (!loopStarted) {
+      loopStarted = true;
+      requestAnimationFrame((t) => {
+        timeOfLastFrame = t;
+        requestAnimationFrame(autoClick);
+      });
+    }
+
     counterElement.textContent = Math.floor(counter).toString();
-    upgradeBtn.textContent = `💓 Rapid Fire (Currently +${growthRate}/sec)`;
+    upgradeBtnA.textContent = `💓 Upgrade B (Currently +${growthRate}/sec)`;
+  }
+});
+
+upgradeBtnC.addEventListener("click", () => {
+  if (counter >= 1000) {
+    counter -= 1000;
+    growthRate++;
+
+    if (!loopStarted) {
+      loopStarted = true;
+      requestAnimationFrame((t) => {
+        timeOfLastFrame = t;
+        requestAnimationFrame(autoClick);
+      });
+    }
+
+    counterElement.textContent = Math.floor(counter).toString();
+    upgradeBtnA.textContent = `💓 Upgrade C (Currently +${growthRate}/sec)`;
   }
 });
